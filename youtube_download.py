50c9bcf81e6eaa2d0f4bc5f3c272b66ae0bebed9
Juliano Lopes <juliano.co.lopes@gmail.com>
2022-10-08 16:00:07 -0300

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -10,12 +10,12 @@ print("Title: ",yt.title)
 print("Number of views: ",yt.views)
 print("Length of video: ",yt.length)
 print("Rating of video: ",yt.rating)
-#Getting the highest resolution possible
-#ys = yt.streams.get_highest_resolution()
 
-# = 0
-#for detail in yt.streams:
-#    i += 1
+
+
+
+
+
 
 
 print("\nType which of them you want to download:\n")
