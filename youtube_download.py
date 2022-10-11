49f15dff2b677a6312a616c05a58434a41d06549
Juliano <juliano.co.lopes@gmail.com>
2022-10-10 22:27:23 -0300

    update youtube_download and requirements

--- youtube_download.py
+++ youtube_download.py
@@ -1,6 +1,8 @@
-
+import colorama
+from colorama import Fore, Style
 from pytube import YouTube
 import time
+colorama.init(autoreset=True)
 
 link = input("Enter the link: ")
 yt = YouTube(link)
@@ -18,22 +20,41 @@ print("Rating of video: ",yt.rating)
 
 
 
-print("\nType which of them you want to download:\n")
-for i in range(len(yt.streams)):
-    print(f"{i + 1} => {yt.streams[i].mime_type}   -   {yt.streams[i].abr if yt.streams[i].abr else yt.streams[i].resolution}")
-    time.sleep(0.5)
+def list_options():
+    print("\nType which of them you want to download:\n")
+    for i in range(len(yt.streams)):
+        print(f"{i + 1} => {yt.streams[i].mime_type}   -   {yt.streams[i].abr if yt.streams[i].abr else yt.streams[i].resolution}")
+        time.sleep(0.3)
 
-your_choice = int(input("\nNumber: "))
-
-try:
-    if your_choice > 0:
-        ys = yt.streams[your_choice - 1]
+    your_choice = int(input("\nNumber: "))
+    return your_choice
 
 
-        #Starting download
-        print("\nDownloading...")
-    ys.download()
-    print("Download completed!!\n")
-except:
-    print("Your choice doesn't exists")
+
+cancel = 0
+while True:
+
+    try:
+        if cancel > 2:
+            cancel_confirm = input("Deseja cancelar? Y/N ")
+            cancel_confirm = cancel_confirm.lower()
+
+            while cancel_confirm != "y" and cancel_confirm != "n":
+                cancel_confirm = input("Deseja cancelar? Y/N ")
+                cancel_confirm = cancel_confirm.lower()
+            if cancel_confirm == "y":
+                print("Cancelado com sucesso\n")
+                break
+        y_c = list_options()
+        if y_c > 0:
+            ys = yt.streams[y_c - 1]
+
+            #Starting download
+            print(Fore.RED + "\nDownloading...")
+        ys.download()
+        print(Fore.GREEN + "Download completed!!\n")
+        break
+    except:
+        print("Your choice doesn't exists")
+    cancel += 1
 
